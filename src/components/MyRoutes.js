65a28e355d17ae8190cec5871f7eba23f95c02ea
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-31 20:48:11 +0700

    User

---
+++ src/components/MyRoutes.js
@@ -0,0 +1,29 @@
+// Route for autheticated user
+import React from 'react';
+import {
+    Route, Redirect
+} from 'react-router-dom';
+
+export const AuthRoute = ({ component: Component, user, ...rest }) => (
+    <Route {...rest} render={props => (
+        user ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{
+                pathname: '/signin',
+            }} />
+        )
+    )} />
+);
+
+export const NotAuthRoute = ({ component: Component, user, ...rest }) => (
+    <Route {...rest} render={props => (
+        user ? (
+            <Redirect to={{
+                pathname: '/user',
+            }} />
+        ) : (
+            <Component {...props} />
+        )
+    )} />
+);
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class User extends Component {
+    render() {
+        return (
+            <div>
+                User Component
+            </div>
+        );
+    }
+}
